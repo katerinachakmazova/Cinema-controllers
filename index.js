3d68b735bd3e9af3979ab6f0b745afb0899f10ca
Катерина Чакмазова <chakmazova06@gmail.com>
2025-10-03 01:23:29 +0300

    Create server on express

--- index.js
+++ index.js
@@ -1,35 +1,66 @@
 const http = require('http');
 const fs = require('fs');
-const HOST_NAME = '127.0.0.1';
-const PORT = 5000;
-// const {summ} = require('./source');
-// console.log(summ(10,20))
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  console.log(`URL is ${url}`);
-  console.log(`METHOD is ${req.method}`);
+// =========================
+const express = require('express');
 
-  res.setHeader('Content-Type', 'text/html; charset = utf-8');
-  // res.write('<h1>SErver</h1>')
-  // res.end()
-  switch (url) {
-    case '/':
-      console.log('Home page');
-      const homepage = fs.readFileSync('./public/index.html');
-      res.write(homepage);
-      res.end();
-      break;
-    case '/form':
-      console.log('Form')
-      const form = fs.readFileSync('./public/form.html');
-      res.write(form);
-      res.end()
-      break;
-    default:
-      res.write('<h1>Page not found</h1>');
-      res.end();
-  }
+const app = express();
+const HOST_NAME = '127.0.0.1';
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(app) 
+// !!!!!!!!
+
+
+app.get('/', (req, res) => {
+  fs.readFile('./public/index.html', 'utf8', (error, data) => {
+    if (error) {
+      res.status(404);
+      throw err;
+    }
+    res.set('Content-Type', 'text/html').send(data);
+  });
 });
+app.get('/form', (req, res) => {
+  fs.readFile('./public/form.html', 'utf8', (error, data) => {
+    if (error) {
+      res.status(404);
+      throw err;
+    }
+    res.set('Content-Type', 'text/html').send(data);
+  });
+});
+
 server.listen(PORT, HOST_NAME, () => {
   console.log(`Server running at http://${HOST_NAME}:${PORT}`);
 });
+// const {summ} = require('./source');
+// console.log(summ(10,20))
+// const server = http.createServer((req, res) => {
+//   const url = req.url;
+//   console.log(`URL is ${url}`);
+//   console.log(`METHOD is ${req.method}`);
+
+//   res.setHeader('Content-Type', 'text/html; charset = utf-8');
+//   // res.write('<h1>SErver</h1>')
+//   // res.end()
+//   switch (url) {
+//     case '/':
+//       console.log('Home page');
+//       const homepage = fs.readFileSync('./public/index.html');
+//       res.write(homepage);
+//       res.end();
+//       break;
+//     case '/form':
+//       console.log('Form')
+//       const form = fs.readFileSync('./public/form.html');
+//       res.write(form);
+//       res.end()
+//       break;
+//     default:
+//       res.write('<h1>Page not found</h1>');
+//       res.end();
+//   }
+// });
+// server.listen(PORT, HOST_NAME, () => {
+//   console.log(`Server running at http://${HOST_NAME}:${PORT}`);
+// });
