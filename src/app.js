18b22f16f20ebfd0d4b624610683be9a3991c911
Катерина Чакмазова <chakmazova06@gmail.com>
2025-10-06 15:21:11 +0300

    Delete unnecessary files

--- src/app.js
+++ src/app.js
@@ -10,7 +10,6 @@ const {errorHandlers: {errorHandler, validationErrorHandler}, time: {getTime, sh
 const app = express();
 app.use(cors())
 app.use(express.json());
-app.use(express.static(path.resolve( 'public')));
 app.use(validationErrorHandler, errorHandler)
 app.use(router);
 app.use('/time', getTime, showTime);
